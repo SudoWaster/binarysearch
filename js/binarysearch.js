32f497ceeca37fc1b5ff7320183c5277fff1a334
SudoWaster <cezaryre@me.com>
2015-09-02 22:43:54 +0200

    Finished binarysearch.js

--- js/binarysearch.js
+++ js/binarysearch.js
@@ -8,9 +8,38 @@ var BinarySearch = function() {
 BinarySearch.prototype = {
 	guess : function() {
 		this.resetValues();
+		
+		while(this.isNotGuessed()) {
+			this.setNewGuessedNumbers();
+		}
+		
+		return this.left;
 	},
+	
 	resetValues : function() {	
 		this.left = MIN_SECTION;
 		this.right = MAX_SECTION;
+	},
+	
+	isNotGuessed : function() {
+		return (this.left < this.right);
+	},
+	
+	setNewGuessedNumbers : function() {
+		this.middle = parseInt((this.left + this.right)/2);
+		
+		if(this.isSectionRight()) {
+			this.right = this.middle;
+		} else {
+			this.left = this.middle + 1;
+		}
+		
+		return this.left;
+	},
+	
+	isSectionRight : function() {
+		var sort_question = "<" + this.left + ", " + this.middle + ">?";
+		
+		return (confirm(sort_question));
 	}
 };
