3b71b0ba84c96d38c46762ef556b4772b9e87844
SudoWaster <cezaryre@me.com>
2015-09-02 22:51:34 +0200

    binarysearch.js comments and html test

--- js/binarysearch.js
+++ js/binarysearch.js
@@ -1,11 +1,28 @@
+/*
+===================
+ CONSTANTS
+===================
+*/
 var MAX_SECTION = 1000;
 var MIN_SECTION = 0;
 
+/*
+===================
+ CONSTRUCTOR
+===================
+*/
 var BinarySearch = function() {
 	this.resetValues();
 };
 
+/*
+===================
+ METHODS
+===================
+*/
 BinarySearch.prototype = {
+	
+	// guess() - start asking loop
 	guess : function() {
 		this.resetValues();
 		
@@ -16,15 +33,18 @@ BinarySearch.prototype = {
 		return this.left;
 	},
 	
+	// resetValues() - get a fresh set of section values
 	resetValues : function() {	
 		this.left = MIN_SECTION;
 		this.right = MAX_SECTION;
 	},
 	
+	// isNotGuessed() - check if have guessed yet
 	isNotGuessed : function() {
 		return (this.left < this.right);
 	},
 	
+	// setNewGuessedNumbers() - get new middle and check if right, then correct section
 	setNewGuessedNumbers : function() {
 		this.middle = parseInt((this.left + this.right)/2);
 		
@@ -33,10 +53,9 @@ BinarySearch.prototype = {
 		} else {
 			this.left = this.middle + 1;
 		}
-		
-		return this.left;
 	},
 	
+	// isSectionRight() - ask user if it's a guess
 	isSectionRight : function() {
 		var sort_question = "<" + this.left + ", " + this.middle + ">?";
 		
